93c68301313d9f714b12fe87b92f9eda2cc325bd
Jagdish587 <t.jag587@gmail.com>
2018-08-15 01:54:13 -0700

    changed file name

---
+++ future_and_promise.cpp
@@ -0,0 +1,60 @@
+// thread_stuff.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include <iostream>
+#include <thread>
+#include <string>
+#include <future>
+using namespace std;
+
+#if 0
+void my_thread_function(int val,string str)
+{
+	cout << "This is thread fiucntion \n";
+	cout << "int val = "<< val<<" string value = "<< str<<endl;
+
+}
+#endif
+
+void div_func(promise<int> promise_obj_div, int a, int b)
+{
+	promise_obj_div.set_value(b/a);
+}
+
+void product_func(promise<int> promise_obj_prod, int a, int b)
+{
+	promise_obj_prod.set_value(a*b);
+}
+
+int main()
+{
+	cout << "This is main function \n";
+	int a = 2;
+	int b = 4;
+
+// declare promise obj 
+	promise<int> promise_obj_prod;
+	promise<int> promise_obj_div;
+
+	future<int> future_obj_prod = promise_obj_prod.get_future();
+	future<int> future_obj_div = promise_obj_div.get_future();
+
+	thread thread_1(product_func, move(promise_obj_prod), a, b);
+	thread thread_2(div_func, move(promise_obj_div), a, b);
+
+	cout << "product value = " << future_obj_prod.get()<<endl;
+	cout << "div value " << future_obj_div.get()<<endl;
+
+	thread_1.join();
+	thread_2.join();
+
+#if 0
+	thread t1(my_thread_function,20,"jagdish");
+	t1.join();
+#endif
+
+    return 0;
+}
+
+
